7b3239d5c2204a42ebfd564faad41c7be589f2d8
boonruang <scubatoy@gmail.com>
2022-02-08 22:00:24 +0700

    Ep20 Header and Menu Intro

---
+++ components/layouts/menu.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Menu({}: Props) {
+  return (
+    <div>
+      <h1>Menu</h1>
+    </div>
+  )
+}
--- pages/stock.tsx
+++ pages/stock.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Header from '../components/layouts/header'
+import Menu from '../components/layouts/menu'
 
 type Props = {}
 
 export default function Stock({}: Props) {
   return (
     <div>
+      <Header />
+      <Menu />
       <h1>Stock</h1>
     </div>
   )
