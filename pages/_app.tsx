753b0e955efe5f4e255130d9dce64aab78fa1c21
boonruang <scubatoy@gmail.com>
2022-02-17 13:24:50 +0700

    Ep95 JWT #7 Set Dispatch in Axios Intercept - add setInterceptor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,13 @@ import App from 'next/app'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { wrapper } from '../redux'
+import { setInterceptor } from '../utils/httpClient'
+import { useDispatch } from 'react-redux'
 
 type Props = {}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  setInterceptor(useDispatch())
   return (
     <>
       <Component {...pageProps} />
--- utils/httpClient.ts
+++ utils/httpClient.ts
@@ -2,36 +2,39 @@ import axios from 'axios'
 import { kToken } from './constants'
 import { getCookie, removeCookie } from './cookie'
 import Router from 'next/router'
-import { useDispatch } from 'react-redux'
+import actions from '../redux/actions'
 
 const httpClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_APP_BASE_API_URL,
 })
 
-httpClient.interceptors.request.use((req) => {
-  // const token = localStorage.getItem(kToken)
-  const token = getCookie(kToken)
-  //NOTE add token
-  if (token) req.headers = { 'x-access-token': token }
-  return req
-})
+export const setInterceptor = (dispatch) => {
+  httpClient.interceptors.request.use((req) => {
+    // const token = localStorage.getItem(kToken)
+    const token = getCookie(kToken)
+    //NOTE add token
+    if (token) req.headers = { 'x-access-token': token }
+    return req
+  })
 
-httpClient.interceptors.response.use(
-  function (response) {
-    return response
-  },
-  function (error) {
-    if (
-      403 === error.response.status ||
-      401 === error.response.status ||
-      500 === error.response.status
-    ) {
-      // removeCookie(kToken)
-      // Router.push('/login')
-    } else {
-      return Promise.reject(error)
-    }
-  },
-)
+  httpClient.interceptors.response.use(
+    function (response) {
+      return response
+    },
+    function (error) {
+      if (
+        403 === error.response.status ||
+        401 === error.response.status ||
+        500 === error.response.status
+      ) {
+        // removeCookie(kToken)
+        // Router.push('/login')
+        dispatch(actions.logout())
+      } else {
+        return Promise.reject(error)
+      }
+    },
+  )
+}
 
 export default httpClient
